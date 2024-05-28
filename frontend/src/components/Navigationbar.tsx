f00a849303612231bc2f83b6bc40f42cee8624fd
Deetlef Koen <u20577304@tuks.co.za>
2024-05-28 13:06:14 +0200

    setup done

--- frontend/src/components/Navigationbar.tsx
+++ frontend/src/components/Navigationbar.tsx
@@ -1,5 +1,5 @@
 import { Navbar, Nav, Container} from 'react-bootstrap';
-import {FaMap, FaUser, FaUsers } from 'react-icons/fa';
+import { FaMap, FaUser, FaUsers } from 'react-icons/fa';
 import { FaHouse } from 'react-icons/fa6';
 import { LinkContainer } from 'react-router-bootstrap';
 import '../assets/styles/nav.css';
@@ -15,22 +15,22 @@ const Navigationbar = () => {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
                     <LinkContainer to="/">
-                        <Nav.Link>
+                        <Nav.Link className="mx-2">
                             <FaHouse/>Home
                         </Nav.Link>
                     </LinkContainer>
                     <LinkContainer to="/maps">
-                            <Nav.Link>
+                            <Nav.Link className="mx-2">
                                 <FaMap/> Maps
                             </Nav.Link>
                         </LinkContainer>
                         <LinkContainer to="/groups">
-                            <Nav.Link>
+                            <Nav.Link className="mx-2">
                                 <FaUsers/>Groups
                             </Nav.Link>
                         </LinkContainer>
                         <LinkContainer to="/login">
-                            <Nav.Link>
+                            <Nav.Link className="mx-2">
                                 <FaUser/> Sign In
                             </Nav.Link>
                         </LinkContainer>
