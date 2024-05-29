013a3fb5bb77dd717b48d47ef9fb19193b9415b7
lukecn03 <lcnobrega03@gmail.com>
2024-05-29 16:18:07 +0200

    Added temp component file

---
+++ frontend/src/components/TempProfileComponents.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+
+const postsGrid = () => {
+    return (
+        <div className="grid-container">
+            <div className="grid-item">Item 1</div>
+            <div className="grid-item">Item 2</div>
+            <div className="grid-item">Item 3</div>
+            <div className="grid-item">Item 4</div>
+            <div className="grid-item">Item 5</div>
+            <div className="grid-item">Item 6</div>
+            <div className="grid-item">Item 7</div>
+            <div className="grid-item">Item 8</div>
+            <div className="grid-item">Item 9</div>
+            <div className="grid-item">Item 10</div>
+            <div className="grid-item">Item 11</div>
+            <div className="grid-item">Item 12</div>
+        </div>
+    )
+}
+
+export default postsGrid;
+
+
