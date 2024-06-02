9204f10b6263baa8b3baa9b342f13c0a210fc10a
Mihir-Arjun <u21458759@tuks.co.za>
2024-06-02 14:14:37 +0200

    Groups can now be created

--- backend/Lookout/src/main/kotlin/com/lookout/Lookout/LookoutApplication.kt
+++ backend/Lookout/src/main/kotlin/com/lookout/Lookout/LookoutApplication.kt
@@ -1,5 +1,6 @@
 package com.lookout.Lookout
 
+import io.github.cdimascio.dotenv.Dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,5 +8,12 @@ import org.springframework.boot.runApplication
 class LookoutApplication
 
 fun main(args: Array<String>) {
+	val dotenv = Dotenv.configure()
+		.directory("../backend/Lookout/.env")
+		.load()
+	dotenv["DB_URL"]?.let { System.setProperty("DB_URL", it) }
+	dotenv["DB_USER"]?.let { System.setProperty("DB_USER", it) }
+	dotenv["DB_PASS"]?.let { System.setProperty("DB_PASS", it) }
 	runApplication<LookoutApplication>(*args)
 }
+
