561890ac853131edcbc92aaacf31651ebd0c9d48
lukecn03 <lcnobrega03@gmail.com>
2024-06-08 12:28:20 +0200

    Added PostController

---
+++ backend/Lookout/src/main/kotlin/com/lookout/Lookout/controller/PostController.kt
@@ -0,0 +1,53 @@
+package com.lookout.Lookout.controller
+
+
+import com.lookout.Lookout.entity.Groups
+import com.lookout.Lookout.entity.User
+import com.lookout.Lookout.entity.Posts
+import com.lookout.Lookout.service.PostService
+//import com.lookout.Lookout.service.UserService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
+
+@RestController
+@RequestMapping("/api/posts")
+class PostController(private val postService: PostService) {
+
+
+    // Create a post
+    @PostMapping
+    fun createPost(@RequestBody post: Posts): ResponseEntity<Groups> {
+        try {
+            val savedPost = postService.save(post)
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedPost)
+        } catch (e: IllegalArgumentException) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null)
+        }
+    }
+
+    // Delete a post
+
+
+    // Get all posts
+    @GetMapping
+    fun getAllPosts(
+        @RequestParam(defaultValue = "0") page: Int,
+        @RequestParam(defaultValue = "10") size: Int
+    ): ResponseEntity<Page<Posts>> {
+        val pageable: Pageable = PageRequest.of(page, size)
+        val posts = postService.findAll(pageable)
+        return ResponseEntity.ok(posts)
+    }
+
+    // Get post by User Id
+    // Get post by Group Id
+    // Update a post
+
+
+
+
+}
