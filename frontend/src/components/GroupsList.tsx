b3d9f03df3fc795a1576ea5e4f9d6b1edecb51c3
aliyahlimbada <131686630+aliyahlimbada@users.noreply.github.com>
2024-06-12 18:36:03 +0200

    integrated users posts for profile page

--- frontend/src/components/GroupsList.tsx
+++ frontend/src/components/GroupsList.tsx
@@ -22,9 +22,12 @@ const GroupsList: React.FC = () => {
       headers: {
         'Accept': 'application/json',
       },
-    })
+    }) 
       .then((response) => response.json())
-      .then((data) => setGroups(data))
+      .then((data) => {
+        console.log(data); // Log the data to check the response format
+        setGroups(data);
+      })
       .catch((error) => console.error('Error fetching groups:', error));
   }, []);
 
