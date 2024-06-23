c9c48f34ee5582a550f4feba7e3d14ae0dc66652
Mihir-Arjun <u21458759@tuks.co.za>
2024-06-23 15:08:26 +0200

    Finished all the unit tests for the get requests for groups

--- backend/Lookout/src/test/kotlin/com/lookout/Lookout/LookoutApplicationTests.kt
+++ backend/Lookout/src/test/kotlin/com/lookout/Lookout/LookoutApplicationTests.kt
@@ -4,8 +4,13 @@ import io.github.cdimascio.dotenv.Dotenv
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
 
 @SpringBootTest(
 	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -15,10 +20,15 @@ import org.springframework.jdbc.core.JdbcTemplate
 		"spring.datasource.password=\${DB_PASS}"
 	]
 )
+
+@AutoConfigureMockMvc
 class LookoutApplicationTests {
 
     @Autowired
     private lateinit var jdbcTemplate: JdbcTemplate
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
 
     @Test
 	fun contextLoads() {
@@ -28,6 +38,24 @@ class LookoutApplicationTests {
     fun `database connection test`() {
         val result = jdbcTemplate.queryForObject("SELECT 1", Int::class.java)
         assert(result == 1)
+    }
+
+    @Test
+    fun `get all groups test`() {
+        mockMvc.perform(get("/api/groups"))
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    fun `get group by id test`() {
+        mockMvc.perform(get("/api/groups/2"))
+            .andExpect(status().isOk)
+    }
+
+    @Test
+    fun `Get all groups with pagination`() {
+        mockMvc.perform(get("/api/groups?page=0&size=10"))
+            .andExpect(status().isOk)
     }
 
     companion object {
