30bb9912338fe522354d8e1907695dc0c3edacdb
Mihir-Arjun <u21458759@tuks.co.za>
2024-06-23 12:32:24 +0200

    Saving progress before updating the branch

---
+++ backend/Lookout/src/test/kotlin/com/lookout/Lookout/GroupTests.kt
@@ -0,0 +1,11 @@
+package com.lookout.Lookout
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class GroupTests {
+    @Test
+    fun `some maths test`() {
+        assertThat(1+2).isEqualTo(3)
+    }
+}
--- backend/Lookout/src/test/kotlin/com/lookout/Lookout/LookoutApplicationTests.kt
+++ backend/Lookout/src/test/kotlin/com/lookout/Lookout/LookoutApplicationTests.kt
@@ -1,13 +1,38 @@
 package com.lookout.Lookout
 
+import io.github.cdimascio.dotenv.Dotenv
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.jdbc.core.JdbcTemplate
 
-@SpringBootTest
+@SpringBootTest(
+	webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+	properties = [
+		"spring.datasource.url=\${DB_URL}",
+		"spring.datasource.username=\${DB_USER}",
+		"spring.datasource.password=\${DB_PASS}"
+	]
+)
 class LookoutApplicationTests {
 
-	@Test
+    @Test
 	fun contextLoads() {
 	}
+
+    companion object {
+        @JvmStatic
+        @BeforeAll
+        fun setup(): Unit {
+            val dotenv = Dotenv.configure()
+                .directory("./.env")
+                .load()
+            dotenv["DB_URL"]?.let { System.setProperty("DB_URL", it) }
+            dotenv["DB_USER"]?.let { System.setProperty("DB_USER", it) }
+            dotenv["DB_PASS"]?.let { System.setProperty("DB_PASS", it) }
+        }
+    }
+
 
 }
