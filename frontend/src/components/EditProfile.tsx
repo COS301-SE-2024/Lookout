96253c2aad76c431a9d4d8eed1849a52561b4a8d
Troy C <u21436933@tuks.co.za>
2024-07-14 21:24:32 +0200

    Frontend for profile settings

---
+++ frontend/src/components/EditProfile.tsx
@@ -0,0 +1,109 @@
+import React, { useMemo, useRef, useState } from "react";
+import { FaPlus } from "react-icons/fa";
+export const EditProfile = () => {
+	const [picture, setPicture] = useState("");
+
+	const fileInputRef = React.useRef<HTMLInputElement | null>(null);
+	const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const file = event.target.files?.[0];
+		if (file) {
+			const fileUrl = URL.createObjectURL(file);
+			setPicture(fileUrl);
+		}
+	};
+	const handleAddPhotoClick = () => {
+		if (fileInputRef.current) {
+			fileInputRef.current.click();
+		}
+	};
+
+	const modalStyles = {
+		modalContainer:
+			"fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-start md:justify-center items-center",
+		modalContent:
+			"bg-bkg rounded-lg p-8 relative w-11/12 md:w-96 h-full md:h-auto overflow-y-auto",
+		closeButton: "absolute top-2 left-2 cursor-pointer",
+		closeIcon: "text-gray-500 hover:text-gray-700",
+		settingsTitle: "text-xl font-semibold mb-4 ml-8",
+		sectionTitle: "text-lg font-semibold mt-4 mb-2",
+		logoutButton: "bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full"
+	};
+
+	return (
+		<>
+			<h2 className={modalStyles.settingsTitle}>Edit Profile</h2>
+			<div className="flex items-center">
+				<ul>
+					<li className="flex flex-col py-2 border-t border-b">
+						<div className="flex justify-center mb-3">
+							<button
+								className="flex items-center justify-center w-12 h-12 border border-gray-300 rounded-lg"
+								onClick={handleAddPhotoClick}
+								data-testid="add-photo-button"
+							>
+								<FaPlus />
+							</button>
+						</div>
+
+						<div className="text-center mb-3">
+							<span className="text-lg">Change Profile Pic</span>
+							<input
+								type="file"
+								accept="image/jpeg, image/png"
+								style={{ display: "none" }}
+								ref={fileInputRef}
+								data-testid="file-input"
+								onChange={handleFileChange}
+							/>
+							{picture && (
+								<img
+									src={picture}
+									alt="Selected"
+									className="w-32 h-32 mt-2 mx-auto"
+								/>
+							)}
+						</div>
+					</li>
+
+					<li className="flex flex-col py-2 border-t border-b">
+						<label
+							htmlFor="name"
+							className="block text-gray-700 font-bold mb-2"
+						>
+							Change Username
+						</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							placeholder="change your username"
+							className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+						/>
+					</li>
+
+					<li className="flex flex-col py-2 border-t border-b">
+						<label
+							htmlFor="name"
+							className="block text-gray-700 font-bold mb-2"
+						>
+							Change Email
+						</label>
+						<input
+							type="text"
+							id="name"
+							name="name"
+							placeholder="change your username"
+							className="border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+						/>
+					</li>
+					<li className="flex flex-col py-2 border-t border-b">
+						<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300 ease-in-out">
+							Reset Password
+						</button>
+					</li>
+				</ul>
+			</div>
+		</>
+	);
+};
+export default EditProfile;
--- frontend/src/components/SettingsModal.tsx
+++ frontend/src/components/SettingsModal.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import { FaChevronRight, FaTimes } from "react-icons/fa";
+import React, { useMemo, useRef, useState } from "react";
+import { FaChevronRight, FaTimes, FaChevronLeft } from "react-icons/fa";
 import ToggleButton from "./ThemeToggleButton";
-import { getEmailFromLocalStorage } from '../utils/auth';
-import HelpCentreModal from './HelpModal';
+import { getEmailFromLocalStorage } from "../utils/auth";
+import HelpCentreModal from "./HelpModal";
+import EditProfile from "./EditProfile";
 
 interface SettingsModalProps {
 	onClose: () => void;
@@ -77,66 +78,124 @@ const SettingsModal: React.FC<SettingsModalProps> = ({ onClose }) => {
 		setShowHelpCentre(false);
 	};
 
+	const [selectedIndex, setSelectedIndex] = useState<undefined | number>(
+		undefined
+	);
+	const jsx = useMemo(() => {
+		if (selectedIndex === undefined) {
+			return undefined;
+		}
+		if (selectedIndex === 0) {
+			return (
+				<div className={modalStyles.modalContainer}>
+					<div className={modalStyles.modalContent}>
+						<div
+							className={modalStyles.closeButton}
+							onClick={() => {
+								setSelectedIndex(undefined);
+							}}
+						>
+							<FaChevronLeft
+								className={modalStyles.closeIcon}
+								size={24}
+							/>
+						</div>
+						<EditProfile />
+					</div>
+				</div>
+			);
+		}
+	}, [selectedIndex]);
+
 	return (
 		<>
 			<div className={modalStyles.modalContainer}>
-				<div className={modalStyles.modalContent}>
-					<div className={modalStyles.closeButton} onClick={onClose}>
-						<FaTimes className={modalStyles.closeIcon} size={24} />
-					</div>
-					<h2 className={modalStyles.settingsTitle}>Settings</h2>
-					<ul>
-						{settings.map((setting, index) => (
-							<li
-								key={index}
-								className="py-2 border-t border-b flex items-center justify-between"
-							>
-								<div className="flex items-center">
-									{setting}
-									{setting === "Dark Theme" && (
-										<label className="ml-48 flex items-center">
-											<ToggleButton onToggle={handleToggle} initialState={isDarkTheme}/>
-										</label>
-									)}
-								</div>
-								{setting !== "Dark Theme" && (
-									<FaChevronRight
-										className="text-gray-400"
-										size={18}
-									/>
-								)}
-							</li>
-						))}
-						<div className="py-2 border-t border-b flex items-center">
-							<h2 className={modalStyles.sectionTitle}>Support</h2>
-						</div>
-						<li 
-							className="py-2 border-t border-b flex items-center justify-between"
-							onClick={handleHelpCentreClick}
+				{jsx ? (
+					jsx
+				) : (
+					<div className={modalStyles.modalContent}>
+						<div
+							className={modalStyles.closeButton}
+							onClick={onClose}
 						>
-							<div className="flex items-center">Help</div>
-							<FaChevronRight className="text-gray-400" size={18} />
-						</li>
-						<li className="py-2 border-t border-b flex items-center justify-between">
-							<div className="flex items-center">
-								Terms of Service
+							<FaTimes
+								className={modalStyles.closeIcon}
+								size={24}
+							/>
+						</div>
+						<h2 className={modalStyles.settingsTitle}>Settings</h2>
+						<ul>
+							{settings.map((setting, index) => (
+								<li
+									key={index}
+									onClick={() => setSelectedIndex(index)}
+									className="py-2 border-t border-b flex items-center justify-between"
+								>
+									<div className="flex items-center">
+										{setting}
+										{setting === "Dark Theme" && (
+											<label className="ml-48 flex items-center">
+												<ToggleButton
+													onToggle={handleToggle}
+													initialState={isDarkTheme}
+												/>
+											</label>
+										)}
+									</div>
+									{setting !== "Dark Theme" && (
+										<FaChevronRight
+											className="text-gray-400"
+											size={18}
+										/>
+									)}
+								</li>
+							))}
+							<div className="py-2 border-t border-b flex items-center">
+								<h2 className={modalStyles.sectionTitle}>
+									Support
+								</h2>
 							</div>
-							<FaChevronRight className="text-gray-400" size={18} />
-						</li>
-						<li className="py-2 border-t border-b flex items-center justify-between">
-							<div className="flex items-center">Privacy Policy</div>
-							<FaChevronRight className="text-gray-400" size={18} />
-						</li>
-					</ul>
-					<button
-						className={modalStyles.logoutButton}
-						onClick={handleLogout}
-					>
-						Logout
-					</button>
-				</div>
+							<li
+								className="py-2 border-t border-b flex items-center justify-between"
+								onClick={handleHelpCentreClick}
+							>
+								<div className="flex items-center">Help</div>
+								<FaChevronRight
+									className="text-gray-400"
+									size={18}
+								/>
+							</li>
+							<li className="py-2 border-t border-b flex items-center justify-between">
+								<div className="flex items-center">
+									Terms of Service
+								</div>
+								<FaChevronRight
+									className="text-gray-400"
+									size={18}
+								/>
+							</li>
+							<li className="py-2 border-t border-b flex items-center justify-between">
+								<div className="flex items-center">
+									Privacy Policy
+								</div>
+								<FaChevronRight
+									className="text-gray-400"
+									size={18}
+								/>
+							</li>
+						</ul>
+						<button
+							className={modalStyles.logoutButton}
+							onClick={handleLogout}
+						>
+							Logout
+						</button>
+					</div>
+				)}
 			</div>
-			{showHelpCentre && <HelpCentreModal onClose={handleCloseHelpCentre} />}
+			{showHelpCentre && (
+				<HelpCentreModal onClose={handleCloseHelpCentre} />
+			)}
 		</>
 	);
 };
