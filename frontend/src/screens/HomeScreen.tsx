09e3f0ae700d80aa98cf15d293f1b82903ae0c2b
aliyahlimbada <131686630+aliyahlimbada@users.noreply.github.com>
2024-07-15 09:04:33 +0200

    removed old shortening url code

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -167,37 +167,6 @@ const HomeScreen: React.FC<CreatePostsProps> = ({ onCreatePost }) => {
 
   const fileInputRef = React.useRef<HTMLInputElement | null>(null);
 
-  // const BITLY_API_URL = 'https://api-ssl.bitly.com/v4/shorten';
-  // const BITLY_ACCESS_TOKEN = '425dd74299985478adcf6bf15f02c09b5bcb8af2';
-
-  // const shortenURL = async (longUrl: string) => {
-  //   console.log("long url ",longUrl)
-  //   const config = {
-  //     headers: {
-  //       Authorization: `Bearer ${BITLY_ACCESS_TOKEN}`,
-  //       'Content-Type': 'application/json',
-  //     },
-  //   };
-  
-  //   const data = {
-  //     long_url: longUrl,
-  //   };
-  
-  //   console.log('Request Data:', data);
-  //   console.log('Request Config:', config);
-
-  //   try {
-  //     const response = await axios.post(BITLY_API_URL, data, config);
-  //     return response.data.link;
-  //   } catch (error) {
-  //     console.error('Error shortening URL:', error);
-  //     return null;
-  //   }
-  // };
-  // // Example to shorten url
-  // const longUrl = 'https://example.com/your-long-photo-url';
-  // shortenURL(longUrl).then(shortUrl => console.log('Short URL:', shortUrl));
-
   const getLocation = () => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
