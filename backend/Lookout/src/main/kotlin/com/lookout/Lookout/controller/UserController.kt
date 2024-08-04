c97fde2c85a7abb8793bf5427fe11d8442ff3576
Mihir-Arjun <u21458759@tuks.co.za>
2024-08-04 13:30:40 +0200

    Added the endpoint to be able to update a user's profile picture

--- backend/Lookout/src/main/kotlin/com/lookout/Lookout/controller/UserController.kt
+++ backend/Lookout/src/main/kotlin/com/lookout/Lookout/controller/UserController.kt
@@ -1,16 +1,27 @@
 package com.lookout.Lookout.controller
 
+import com.lookout.Lookout.entity.User
+import com.lookout.Lookout.service.UserService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 
 @RestController
-@RequestMapping("/")
-class UserController {
+@RequestMapping("/api/users")
+class UserController (private val userService: UserService){
     @GetMapping("user")
     fun getUser(): ResponseEntity<String>{
         return ResponseEntity.ok("User Login")
     }
+
+    @PutMapping("/update-profile-pic")
+    fun updateProfilePic(@RequestBody updateProfilePicRequest: UpdateProfilePicRequest): ResponseEntity<User> {
+        val updatedUser = userService.updateProfilePic(updateProfilePicRequest.userId, updateProfilePicRequest.newProfilePicUrl)
+        return ResponseEntity.ok(updatedUser)
+    }
 }
+
+data class UpdateProfilePicRequest(
+    val userId: Long,
+    val newProfilePicUrl: String
+)
