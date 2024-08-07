b7bd077f0dca7b9103dcb2e6fad4a94029d72b37
Troy C <u21436933@tuks.co.za>
2024-08-07 12:28:11 +0200

    Service Worker

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,6 +1,51 @@
+const CACHE_NAME = "offline-cache-v1";
+
 self.addEventListener("install", (event) => {
-    console.log("Service worker installed");
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			return cache.addAll([
+				"/HomeScreen.tsx",
+				"/ExploreScreen.tsx",
+				"/GroupScreen.tsx",
+				"/CategoryPostsPage.tsx",
+				"/LoginScreen.tsx",
+				"/Profile.tsx",
+				"/SignUpScreen.tsx"
+				//Will add more pages here when we need them
+			]);
+		})
+	);
 });
-self.addEventListener("activate", (event) => {
-    console.log("Service worker activated");
+
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then((response) => {
+				if (response) {
+					return response;
+				}
+
+				return fetch(event.request).then((networkResponse) => {
+					if (
+						!networkResponse ||
+						networkResponse.status !== 200 ||
+						networkResponse.type !== "basic"
+					) {
+						return networkResponse;
+					}
+
+					const responseToCache = networkResponse.clone();
+
+					caches.open(CACHE_NAME).then((cache) => {
+						cache.put(event.request, responseToCache);
+					});
+
+					return networkResponse;
+				});
+			})
+			.catch(() => {
+				return caches.match("/offline.html");
+			})
+	);
 });
