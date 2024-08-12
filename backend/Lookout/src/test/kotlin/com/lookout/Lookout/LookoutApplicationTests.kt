78ad2fda58aaa5c10b9e75eb6fecbeed1566c38c
Deetlef Koen <131651521+Rockstar458@users.noreply.github.com>
2024-08-12 06:13:22 +0200

    Update LookoutApplicationTests.kt

--- backend/Lookout/src/test/kotlin/com/lookout/Lookout/LookoutApplicationTests.kt
+++ backend/Lookout/src/test/kotlin/com/lookout/Lookout/LookoutApplicationTests.kt
@@ -588,21 +588,22 @@ class LookoutApplicationTests {
 
 
 companion object {
-     @JvmStatic
-     @BeforeAll
-     fun setup(): Unit {
-         if (System.getenv("GITHUB_ACTIONS") == null) {
-             val dotenv = Dotenv.configure()
-                 .directory("./.env")
-                 .ignoreIfMalformed()
-                 .ignoreIfMissing()
-                 .load()
-             dotenv["DB_URL"]?.let { System.setProperty("DB_URL", it) }
-             dotenv["DB_USER"]?.let { System.setProperty("DB_USER", it) }
-             dotenv["DB_PASS"]?.let { System.setProperty("DB_PASS", it) }
-         }
-     }
- }
+    @JvmStatic
+    @BeforeAll
+    fun setup(): Unit {
+        if (System.getenv("GITHUB_ACTIONS") == null) {
+            val dotenv = Dotenv.configure()
+                .ignoreIfMalformed()
+                .ignoreIfMissing()
+                .load()
+
+            dotenv["DB_URL"]?.let { System.setProperty("DB_URL", it) }
+            //dotenv["DB_USER"]?.let { System.setProperty("DB_USER", it) }
+            //dotenv["DB_PASS"]?.let { System.setProperty("DB_PASS", it) }
+        }
+    }
+}
+
 
 
 
