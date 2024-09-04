f756937b2afa5d476f8563618728de40284be869
Troy C <u21436933@tuks.co.za>
2024-09-04 22:15:41 +0200

    More for landing

--- frontend/src/components/LandingComponent.tsx
+++ frontend/src/components/LandingComponent.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 import "../assets/styles/Landing.css";
 import HeroSplash from "../assets/staticImages/DesignSmartAndLaptop.png";
+import GetStartedButton from "./GetStartedButton";
 
 const LandingComponent = () => {
 	return (
 		<div className="Container">
 			<div className="Hero">
-				<h1 className="heroH1">
-					The modern way to get in touch with Nature.
-				</h1>
+				<div>
+					<h1 className="heroH1">
+						The modern way to get in touch with Nature.
+						<GetStartedButton
+							onClick={function (): void {
+								throw new Error("Function not implemented.");
+							}}
+						/>
+					</h1>
+				</div>
 				<img className="HeroSplash" src={HeroSplash} alt="HeroSplash" />
 			</div>
 		</div>
