218d0d5bb63aedce9d18a228277b7343725358ed
aliyahlimbada <131686630+aliyahlimbada@users.noreply.github.com>
2024-09-18 22:42:47 +0200

    login and signup pages done - logo to be added

--- frontend/src/screens/SignUpScreen.tsx
+++ frontend/src/screens/SignUpScreen.tsx
@@ -12,21 +12,21 @@ const SignUpScreen = () => {
   const [error, setError] = useState('');
 
   const handleLogin = async () => {
-	  try {
-		// Redirect to the backend to initiate Google OAuth
-		window.location.href = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=http://localhost:8080/api/auth/signup/google&response_type=code&client_id=456933252122-r308hq3v8185kb9k4k9cma7q05afbejq.apps.googleusercontent.com&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+openid&access_type=offline";
-	  } catch (error) {
-		navigate("/login");
-		console.error("Error during login:", error);
-	  }
-	};
+    try {
+      // Redirect to the backend to initiate Google OAuth
+      window.location.href = "https://accounts.google.com/o/oauth2/v2/auth?redirect_uri=http://localhost:8080/api/auth/signup/google&response_type=code&client_id=456933252122-r308hq3v8185kb9k4k9cma7q05afbejq.apps.googleusercontent.com&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+openid&access_type=offline";
+    } catch (error) {
+      navigate("/login");
+      console.error("Error during login:", error);
+    }
+  };
 
   useEffect(() => {
-		if (token !== "" && token !== null) {
-			navigate("/");
-			window.location.reload();
-		}
-	}, [token, navigate]);
+    if (token !== "" && token !== null) {
+      navigate("/");
+      window.location.reload();
+    }
+  }, [token, navigate]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -43,7 +43,7 @@ const SignUpScreen = () => {
         },
         body: JSON.stringify({
           email: email,
-          userName:email,
+          userName: email,
           passcode: password,
           role: "ADMIN"
         }),
@@ -54,35 +54,27 @@ const SignUpScreen = () => {
       }
 
       const data = await response.json();
-		  localStorage.setItem('authToken', data.token);
-		  localStorage.setItem('userEmail', email);
-		  setToken(data.token);
+      localStorage.setItem('authToken', data.token);
+      localStorage.setItem('userEmail', email);
+      setToken(data.token);
     } catch (error) {
       console.error('Error:', error);
       setError('Signup failed');
     }
   };
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
-      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
-        <div className="flex justify-center mb-4">
+    <div className="flex items-center justify-center min-h-screen p-4">
+      <div className="w-full max-w-md p-8 space-y-8 rounded-lg border">
+        <div className="text-center mb-2">
+          <h1 className="text-2xl font-bold">Create an Account</h1>
+        </div>
+        <hr></hr>
+        {/* <div className="flex justify-center mb-4">
           <div className="w-56 h-56 flex items-center justify-center">
           <img src="/logo.png" alt="Logo" />
           </div>
-        </div>
-        <div className="flex justify-center">
-          <button className="w-full py-2 text-black bg-white-500 rounded-full hover:bg-gray-300 focus:outline-none flex items-center justify-center border border-gray-300"
-						onClick={handleLogin}
-					>
-						<FcGoogle size={20} style={{ marginRight: 10 }} />
-							Sign in with Google
-					</button>
-        </div>
-        <div className="flex items-center justify-center my-4">
-          <hr className="w-full border-gray-300" />
-          <span className="px-3 text-gray-500">or</span>
-          <hr className="w-full border-gray-300" />
-        </div>
+        </div> */}
+
         <form className="mt-2 space-y-4" onSubmit={handleSubmit}>
           <div>
             <label htmlFor="email" className="block text-sm font-medium text-gray-700">
@@ -96,7 +88,7 @@ const SignUpScreen = () => {
               value={email}
               onChange={(e) => setEmail(DOMPurify.sanitize(e.target.value))}
               required
-              className="w-full px-3 py-2 mt-1 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-full focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+							className="w-full px-3 py-2 mt-1 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-black focus:border-navBkg"
             />
           </div>
           <div>
@@ -111,7 +103,7 @@ const SignUpScreen = () => {
               value={password}
               onChange={(e) => setPassword(DOMPurify.sanitize(e.target.value))}
               required
-              className="w-full px-3 py-2 mt-1 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-full focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+							className="w-full px-3 py-2 mt-1 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-black focus:border-navBkg"
             />
           </div>
           <div>
@@ -125,22 +117,41 @@ const SignUpScreen = () => {
               value={confirmPassword}
               onChange={(e) => setConfirmPassword(DOMPurify.sanitize(e.target.value))}
               required
-              className="w-full px-3 py-2 mt-1 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-full focus:outline-none focus:ring-blue-500 focus:border-blue-500"
+							className="w-full px-3 py-2 mt-1 text-gray-900 placeholder-gray-500 border border-gray-300 rounded-md focus:outline-none focus:ring-black focus:border-navBkg"
             />
           </div>
           {error && <div className="text-red-500">{error}</div>}
           <div>
             <button
               type="submit"
-              className="w-full px-4 py-2 text-white bg-[#6A994E] rounded-full hover:bg-green-600 focus:outline-none"
+							className="w-full px-4 py-2 text-white bg-navBkg rounded-md hover:bg-white hover:text-navBkg border border-navBkg"
             >
               Sign Up
             </button>
           </div>
-          <div className="text-center">
-            Already have an account? <Link to="/login" className="text-blue-500">Login here</Link>
-          </div>
+
         </form>
+
+        <div className="flex items-center my-2">
+					<hr className="flex-grow border-gray-300" />
+					<span className="px-3 text-gray-500 whitespace-nowrap">OR CONTINUE WITH</span>
+					<hr className="flex-grow border-gray-300" />
+				</div>
+
+        <div className="flex justify-center">
+          <button className="w-full py-2 text-black bg-white-500 rounded-md hover:bg-gray-300 focus:outline-none flex items-center justify-center border border-gray-300"
+            onClick={handleLogin}
+          >
+            <FcGoogle size={20} style={{ marginRight: 10 }} />
+            Sign in with Google
+          </button>
+        </div>
+
+        <div className="text-center text-sm">
+          Already have an account? <Link to="/login" className="text-sm text-content underline hover:text-navBkg">Login here</Link>
+        </div>
+
+
       </div>
     </div>
   );
