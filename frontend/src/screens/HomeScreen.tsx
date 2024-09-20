61df81505dfe0e9ed5de69698ccbf18d9898712c
aliyahlimbada <131686630+aliyahlimbada@users.noreply.github.com>
2024-09-21 00:42:47 +0200

    succes modal styling fixed

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1351,7 +1351,7 @@ const HomeScreen: React.FC = () => {
 							Post Created Successfully!
 						</h2>
 						<button
-							className="block mx-auto px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+							className="block mx-auto px-4 py-2 bg-navBkg2 text-nav rounded-md hover:bg-nav hover:text-content hover:border hover:border-content"
 							onClick={() => {
 								setIsSuccessModalOpen(false);
 
