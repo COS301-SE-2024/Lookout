7c0cc90cfa0b3f4dcd893e0b19ccd8ad109d4253
Troy C <u21436933@tuks.co.za>
2024-09-25 16:34:33 +0200

    Uncommented Service Worker

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,50 +1,50 @@
-// const CACHE_NAME = "lookout-v1";
+const CACHE_NAME = "lookout-v1";
 
-// self.addEventListener("fetch", (event) => {
-// 	if (event.request.url.includes("createPost")) {
-// 		return;
-// 	}
+self.addEventListener("fetch", (event) => {
+	if (event.request.url.includes("createPost")) {
+		return;
+	}
 
-// 	event.respondWith(
-// 		caches.match(event.request).then((response) => {
-// 			if (response) {
-// 				return response;
-// 			}
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			if (response) {
+				return response;
+			}
 
-// 			const fetchRequest = event.request.clone();
+			const fetchRequest = event.request.clone();
 
-// 			return fetch(fetchRequest).then((response) => {
-// 				if (
-// 					!response ||
-// 					response.status !== 200 ||
-// 					response.type !== "basic"
-// 				) {
-// 					return response;
-// 				}
+			return fetch(fetchRequest).then((response) => {
+				if (
+					!response ||
+					response.status !== 200 ||
+					response.type !== "basic"
+				) {
+					return response;
+				}
 
-// 				const responseToCache = response.clone();
+				const responseToCache = response.clone();
 
-// 				caches.open(CACHE_NAME).then((cache) => {
-// 					cache.put(event.request, responseToCache);
-// 				});
+				caches.open(CACHE_NAME).then((cache) => {
+					cache.put(event.request, responseToCache);
+				});
 
-// 				return response;
-// 			});
-// 		})
-// 	);
-// });
+				return response;
+			});
+		})
+	);
+});
 
-// self.addEventListener("activate", (event) => {
-// 	const cacheWhitelist = [CACHE_NAME];
-// 	event.waitUntil(
-// 		caches.keys().then((cacheNames) => {
-// 			return Promise.all(
-// 				cacheNames.map((cacheName) => {
-// 					if (cacheWhitelist.indexOf(cacheName) === -1) {
-// 						return caches.delete(cacheName);
-// 					}
-// 				})
-// 			);
-// 		})
-// 	);
-// });
+self.addEventListener("activate", (event) => {
+	const cacheWhitelist = [CACHE_NAME];
+	event.waitUntil(
+		caches.keys().then((cacheNames) => {
+			return Promise.all(
+				cacheNames.map((cacheName) => {
+					if (cacheWhitelist.indexOf(cacheName) === -1) {
+						return caches.delete(cacheName);
+					}
+				})
+			);
+		})
+	);
+});
